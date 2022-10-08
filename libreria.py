4757b5616e580ef7fa45e13cf88490ef9280b5c4
Antoniopv7 <71039689+Antoniopv7@users.noreply.github.com>
2022-10-08 15:55:58 -0300

    Update libreria.py

--- libreria.py
+++ libreria.py
@@ -1,6 +1,13 @@
-import random
+import json
 
-valor = random.randint(0, 10)
-print (valor)
+def ordenamiento_seleccion(lista):
+    for n in range(len(lista) - 1 ,0, -1):
+        posicion_maxima = 0
 
-print("caca seca")
+        for L in range(1, n + 1):
+            if lista[1] > lista[posicion_maxima]:
+                posicion_maxima = 1
+
+        temp = lista[n]
+        lista[n] = lista[posicion_maxima]
+        lista[posicion_maxima] = temp          
