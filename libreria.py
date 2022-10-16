340aa23722426dd99a6c5d451864b3fd22a5ac24
Antoniopv7 <71039689+Antoniopv7@users.noreply.github.com>
2022-10-16 19:06:35 -0300

    avance

--- libreria.py
+++ libreria.py
@@ -1,6 +1,6 @@
 ############################# algoritmo de seleccion
 
-def selectionSort(aList):
+def seleccion(aList):
     for i in range(len(aList)):
         least = i
         for k in range(i+1, len(aList)):
@@ -16,11 +16,6 @@ def swap(A, x, y):
     A[x] = A[y]
     A[y] = temp
 
-print("Ordenamiento por Seleccion")
-my_list = [5.76,4.7,25.3,4.6,32.4,55.3,52.3,7.6,7.3,86.7,43.5]
-selectionSort(my_list)
-print (my_list)
-
 ############################# algoritmo de seleccion
 
 ############################# algoritmo de insercion
@@ -35,17 +30,12 @@ def insercion(A):
                 A[j-1]=aux
     return A
 
-#Programa Principal
-print("Ordenamiento por Insercion")
-A=[6,5,3,1,8,7,2,4]
-insercion(A)
-print(A)
 
 ############################# algoritmo de insercion
 
 ############################# algoritmo de burbuja
 
-def ord_burbuja(arreglo):
+def burbuja(arreglo):
     n = len(arreglo)
 
     for i in range(n-1):       # <-- bucle padre
@@ -53,19 +43,12 @@ def ord_burbuja(arreglo):
             if arreglo[j] > arreglo[j+1]:
                 arreglo[j], arreglo[j+1] = arreglo[j+1], arreglo[j]
 
-print("Ordenamiento por Burbuja")
-elementos = [8, 3, 1, 19, 14]
-ord_burbuja(elementos)
-
-print(elementos)
 
 ############################# algoritmo de burbuja
 
 ############################# algoritmo de quicksort
 
-miLista = [34,93,19,58,12,28,95,37,23,80,57,82,55,48,21,39,53,65,30,32,84,64,44,68,36]
-
-def sort(lista):
+def quicksort(lista):
     izquierda = []
     centro = []
     derecha = []
@@ -79,25 +62,20 @@ def sort(lista):
             elif i > pivote:
                 derecha.append(i)
         #print(izquierda+["-"]+centro+["-"]+derecha)
-        return sort(izquierda)+centro+sort(derecha)
+        return quicksort(izquierda)+centro+quicksort(derecha)
     else:
         return lista
 
-print("Ordenamiento por quicksort")
-print(miLista)
-print(sort(miLista))
 
 ############################# algoritmo de quicksort
 
 ############################# algoritmo de mergesort
 
 def merge_sort(lista):
-
     if len(lista) < 2:
         return lista
-
-    # De lo contrario, se divide en 2
-    else:
+    
+    else:        
         middle = len(lista) // 2
         right = merge_sort(lista[:middle])
         left = merge_sort(lista[middle:])
@@ -105,14 +83,9 @@ def merge_sort(lista):
 
 # Función merge
 def merge(lista1, lista2):
-    """
-    merge se encargara de intercalar los elementos de las dos
-    divisiones.
-    """
     i, j = 0, 0 # Variables de incremento
     result = [] # Lista de resultado
 
-# Intercalar ordenadamente
     while(i < len(lista1) and j < len(lista2)):
         if (lista1[i] < lista2[j]):
             result.append(lista1[i])
@@ -121,44 +94,17 @@ def merge(lista1, lista2):
             result.append(lista2[j])
             j += 1
 
-# Agregamos los resultados a la lista
     result += lista1[i:]
     result += lista2[j:]
 
-    # Retornamos el resultados
     return result
 
-# Prueba del algoritmo
-
-print("Ordenamiento por mergesort")
-lista = [31,3,88,1,4,2,42]
-
-merge_sort_result = merge_sort(lista)
-print(merge_sort_result)
 
 ############################# algoritmo de mergesort
 
 ############################# algoritmo de heapsort
 
-import random
 
-def heap_sort (heap): # Saque el nodo raíz e intercambie con el último bit, y continúe el proceso de ajuste del montón para los primeros nodos len-1.
-    build_max_heap(heap)
-    # El primer elemento de la lista ajustada es el elemento más grande de esta lista, cámbielo por el último elemento y luego ajuste recursivamente la lista restante al montón máximo
-    for i in range(len(heap)-1, -1, -1):
-        heap[0], heap[i] = heap[i], heap[0]
-        max_heapify(heap, i, 0)
-
-# Prueba
-if __name__ == '__main__':
-    a = [30, 50, 57, 77, 62, 78, 94, 80, 84]
-    print(a)
-    heap_sort(a)
-    print(a)
-    b = [random.randint(1,1000) for i in range(1000)]
-    print(b)
-    heap_sort(b)
-    print(b)
 
 
 ############################# algoritmo de heapsort
--- main.py
+++ main.py
@@ -1,2 +1,125 @@
-import libreria
+import libreria as li
+import tkinter as tk
+import random
+from timeit import default_timer
 
+def mostrar_menu(opciones):
+    print('Seleccione una opción:')
+    for clave in sorted(opciones):
+        print(f' {clave}) {opciones[clave][0]}')
+
+7
+def leer_opcion(opciones):
+    while (a := input('Opción: ')) not in opciones:
+        print('Opción incorrecta, vuelva a intentarlo.')
+    return a
+
+
+def ejecutar_opcion(opcion, opciones):
+    opciones[opcion][1]()
+
+
+def generar_menu(opciones, opcion_salida):
+    opcion = None
+    while opcion != opcion_salida:
+        mostrar_menu(opciones)
+        opcion = leer_opcion(opciones)
+        ejecutar_opcion(opcion, opciones)
+        print()
+
+
+def menu_principal():
+    opciones = {
+        '1': ('Ejecutgar Algoritmo de Ordenamiento por Seleccion: ', accion1),
+        '2': ('Ejecutgar Algoritmo de Ordenamiento por Insercion: ', accion2),
+        '3': ('Ejecutgar Algoritmo de Ordenamiento por Burbuja: ', accion3),
+        '4': ('Ejecutgar Algoritmo de Ordenamiento por Quicksort: ', accion4),
+        '5': ('Ejecutgar Algoritmo de Ordenamiento por Mergesort: ', accion5),
+        '6': ('Ejecutgar Algoritmo de Ordenamiento por Heapsort: ', accion6),
+        '7': ('Salir', salir)
+    }
+
+    generar_menu(opciones, '7')
+
+
+def accion1():
+    print('Has elegido la opción 1')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 100, 1) for i in range(int(a))]
+    print (res)
+    tInicial = default_timer()
+    li.seleccion(res)
+    print(res)
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')
+
+
+def accion2():
+    print('Has elegido la opción 2')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 100, 1) for i in range(int(a))]
+    print (res)
+    tInicial = default_timer()
+    li.insercion(res)
+    print(res)
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')
+
+
+def accion3():
+    print('Has elegido la opción 3')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 100, 1) for i in range(int(a))]
+    print (res)
+    tInicial = default_timer()
+    li.burbuja(res)
+    print(res)
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')
+
+
+def accion4():
+    print('Has elegido la opción 4')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 100, 1) for i in range(int(a))]
+    print (res)
+    tInicial = default_timer()
+    print(li.quicksort(res))
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')
+
+
+def accion5():
+    print('Has elegido la opción 5')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 100, 1) for i in range(int(a))]
+    print(res)
+    tInicial = default_timer()
+    print(li.merge_sort(res))
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')   
+
+def accion6():
+    print('Has elegido la opción 6')
+    a = input('Seleccione el rango de la Lista: ')
+    res = [random.randrange(1, 50, 1) for i in range(int(a))]
+    print (res)
+    tInicial = default_timer()
+    li.heap_sort(res)
+    print(res)
+    tFinal = default_timer()
+    ttotal_de_ejecucion_algoritmo = tFinal - tInicial
+    print('El tiempo de ejecucion del algoritmo es de: '+ str(ttotal_de_ejecucion_algoritmo) + ' seg')
+
+
+def salir():
+    print('Saliendo')
+
+
+if __name__ == '__main__':
+    menu_principal()
